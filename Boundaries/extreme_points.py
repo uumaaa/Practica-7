61122fe38cdf5ce89306fffad7c422e46126199d
Alvaro <agarciav2102@alumno.ipn.mx>
2023-11-03 22:55:24 -0600

    Extreme Points Added

---
+++ Boundaries/extreme_points.py
@@ -0,0 +1,18 @@
+def findExtremePoints(boundary):
+    north_point = None
+    south_point = None
+    east_point = None
+    west_point = None
+
+    for point in boundary:
+        y, x = point
+        if north_point is None or y < north_point[0]:
+            north_point = point
+        if south_point is None or y > south_point[0]:
+            south_point = point
+        if east_point is None or x > east_point[1]:
+            east_point = point
+        if west_point is None or x < west_point[1]:
+            west_point = point
+
+    return north_point, south_point, east_point, west_point
